31898c2b88e87ac7e49a4d2a4cba150b3a5ea06f
Meysam Bavi <meybsky@gmail.com>
2021-08-14 01:28:32 +0430

    Completed page 1

---
+++ Assets/Scripts/ResultText.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResultText : MonoBehaviour
+{
+
+    Localize resultTextLocalize;
+    string input;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameObject.SetActive(false);
+        resultTextLocalize = GetComponent<Localize>(); 
+    }
+
+    public void SaveInput(string value)
+    {
+        input = value;
+    }
+
+    public void ConfirmPressed()
+    {
+        resultTextLocalize.SetKey(resultTextLocalize.GetLocalizedString().key, input);
+        gameObject.SetActive(true);
+    }
+}
